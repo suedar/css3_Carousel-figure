0b219568e3c1a80ea1cd6497af8ca0598c681f03
suedar <690372513@qq.com>
2019-08-01 18:05:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HelloWorld>
+      <img src="https://randomm.cdn.bcebos.com/123.jpeg" alt="">
+      <img src="https://randomm.cdn.bcebos.com/123.jpeg" alt="">
+      <img src="https://randomm.cdn.bcebos.com/123.jpeg" alt="">
+    </HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+
+<script>
+export default {
+  name: 'vue-css-cf',
+  data() {
+    return {
+    }
+  },
+  props: {
+      width: {
+          type: Number,
+          default: 300
+      }
+  },
+  render(h) {
+    const { width } = this.$props;
+    const imgs = this.$slots.default.map(img => h('li', [h(img)]));
+    // console.log(imgs)
+    return h('div', {
+      'class': 'vue-css-cf',
+    }, [
+      h('ul', )
+    ])
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
